da477f668f94c88e2f070fbb1babe28edc9ee054
GreatWyrm <alecmusante@gmail.com>
2018-12-01 13:51:28 -0500

    Added PlayerScript and more to PlayerPhysicsScript

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+    private int xPos;
+    private int yPos;
+    private PlayerPhysicsScript playerPhysics;
+
+
+    void Start() {
+        xPos = 0;
+        yPos = 0;
+        playerPhysics = new PlayerPhysicsScript(this);
+    }
+    public void changePlayerY(int deltaY)
+    {
+        yPos += deltaY;
+    }
+    private void playerDash()
+    {
+
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
