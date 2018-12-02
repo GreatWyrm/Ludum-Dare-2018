9f26c7730587a7e4f15d7b2229c83765f27bfd57
GreatWyrm <alecmusante@gmail.com>
2018-12-01 22:41:05 -0500

    Changed Physics to Unity Enginge Physics Added the tutorial level

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -1,97 +1,58 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class PlayerScript : MonoBehaviour {
-
-    private int xPos;
-    private int yPos;
-
-
-    // Player Phyisics Variables
-    public float gravity = 0.01f;
-    public float terminalVelocity = 1.0f;
-    public float horizontalSpeed = 0.2f;
+    private float horizontalSpeed = 0.12f;
 
 
     private float playerFallingSpeed;
     private bool gravityPaused;
     private bool isGravityInverted;
 
-    public bool isDashing;
-    private int dashIntervalLength = 2;
-    private const int numDashIntervals = 50;
+    private bool isDashing;
+    private bool isLanded;
+    private int jumpsLeft;
+    private float dashIntervalLength = 0.8f;
+    private const int numDashIntervals = 15;
     private int numIntervalsDashed;
     private KeyCode jumpKeyCode = KeyCode.Space;
     private KeyCode altJump = KeyCode.W;
     private KeyCode moveRightKeyCode = KeyCode.D;
     private KeyCode moveLeftKeyCode = KeyCode.A;
     private KeyCode fallDownKeyCode = KeyCode.S;
-    private KeyCode dashKeyCode = KeyCode.LeftShift;
+    private KeyCode dashKeyCode = KeyCode.RightShift;
     private float playerMoveX = 0;
 
-    // private boolean touchingVertical() {
-       
-    // }
-
     void Awake() {
-        xPos = 0;
-        yPos = 0;
         isDashing = false;
+        jumpsLeft = 2;
         playerFallingSpeed = 0f;
         gravityPaused = false;
+        isGravityInverted = false;
     }
 
     
     private void playerDash()
     {
-        gravityPaused = true;
+        GetComponent<Rigidbody2D>().gravityScale = 0;
         numIntervalsDashed = 0;
         isDashing = true;
     }
     private void playerJump()
     {
-        float playerJumpStrength = 0.4f;
-        gravityPaused = false;
-        if(isGravityInverted)
+        if (jumpsLeft > 0)
         {
-            playerFallingSpeed = -playerJumpStrength;
-        } else
-        {
-            playerFallingSpeed = playerJumpStrength;
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 10f), ForceMode2D.Impulse);
+            jumpsLeft--;
         }
     }
     private void FixedUpdate()
     {
-        if(isDashing)
-        {
-            if(numIntervalsDashed < numDashIntervals)
-            {
-                xPos += dashIntervalLength;
-                numIntervalsDashed++;
-            } else
-            {
-                isDashing = false;
-                gravityPaused = false;
-            }
-        }
-        if (!gravityPaused)
-        {
-                if (Mathf.Abs(playerFallingSpeed) < terminalVelocity)
-                {
-                    playerFallingSpeed += -gravity;
-                }
-        }
-
-         Vector2 start = new Vector2(transform.position.x, transform.position.y - GetComponent<Collider2D>().bounds.extents.y);
-        RaycastHit2D hit = Physics2D.Raycast(start, Vector2.down, .05f);
-        if (hit.collider != null && playerFallingSpeed != 0)
-        {
-            gravityPaused = true;
-            playerFallingSpeed = 0;
-        }
-
         Vector3 moveVector = new Vector3(playerMoveX, playerFallingSpeed, 0.0f);
         transform.Translate(moveVector);
-        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
     }
     // Update is called once per frame
     void Update () {
@@ -118,6 +79,28 @@ public class PlayerScript : MonoBehaviour {
         if (Input.GetKeyDown(dashKeyCode))
         {
             playerDash();
+        }
+        if(Input.GetKeyUp(moveRightKeyCode) && playerMoveX == horizontalSpeed)
+        {
+            playerMoveX = 0;
+        }
+        if (Input.GetKeyUp(moveLeftKeyCode) && playerMoveX == -horizontalSpeed)
+        {
+            playerMoveX = 0;
+        }
+        if(isDashing)
+        {
+            if(numIntervalsDashed < numDashIntervals)
+            {
+                Vector2 moveVector = new Vector2(transform.position.x + dashIntervalLength, transform.position.y);
+                GetComponent<Rigidbody2D>().MovePosition(moveVector);
+                numIntervalsDashed++;
+            } else
+            {
+                GetComponent<Rigidbody2D>().gravityScale = 1;
+                isDashing = false;
+            }
+
         }
     }
 }
