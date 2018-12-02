0dd691ad295545092a63976afdfa945a4d0b1cb6
GreatWyrm <alecmusante@gmail.com>
2018-12-02 15:14:41 -0500

    Added more of the level and physics changes Dash now works properly in midair Dash cooldown properly set up You can now fall through glass platforms

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -10,6 +10,8 @@ public class PlayerScript : MonoBehaviour {
     private bool isGravityInverted;
 
     private bool isDashing;
+    private bool canDash;
+    private bool canDashCooldown;
     private bool isLanded;
     private int jumpsLeft;
     private float dashIntervalLength = 0.8f;
@@ -22,6 +24,8 @@ public class PlayerScript : MonoBehaviour {
     private KeyCode fallDownKeyCode = KeyCode.S;
     private KeyCode dashKeyCode = KeyCode.RightShift;
     private float playerMoveX = 0;
+    private float timeNoCollideWithGlass;
+
 
     void Awake() {
         isDashing = false;
@@ -29,52 +33,85 @@ public class PlayerScript : MonoBehaviour {
         playerFallingSpeed = 0f;
         gravityPaused = false;
         isGravityInverted = false;
+        canDash = true;
+        canDashCooldown = true;
     }
 
     
     private void playerDash()
     {
-        GetComponent<Rigidbody2D>().gravityScale = 0;
-        numIntervalsDashed = 0;
-        isDashing = true;
+        if(canDash && canDashCooldown)
+        {
+            GetComponent<Rigidbody2D>().gravityScale = 0;
+            numIntervalsDashed = 0;
+            isDashing = true;
+            canDash = false;
+            canDashCooldown = false;
+            StartCoroutine("dashCooldown");
+        }
     }
     private void playerJump()
     {
         if (jumpsLeft > 0)
         {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
             GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 10f), ForceMode2D.Impulse);
             jumpsLeft--;
         }
     }
     private void FixedUpdate()
     {
-        Vector3 moveVector = new Vector3(playerMoveX, playerFallingSpeed, 0.0f);
-        transform.Translate(moveVector);
+        if(!isDashing)
+        {
+            Vector3 moveVector = new Vector3(playerMoveX, playerFallingSpeed, 0.0f);
+            transform.Translate(moveVector);
+        }
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        // We will have to do an if/else for gravity
+        Vector2 bottomOfPlayer = new Vector2(transform.position.x, transform.position.y - GetComponent<Collider2D>().bounds.extents.y);
+        RaycastHit2D detectGround = Physics2D.Raycast(bottomOfPlayer, Vector2.down, 0.125f);
+        if(detectGround.collider != null)
+        {
+            jumpsLeft = 2;
+            canDash = true;
+        }
+
     }
     // Update is called once per frame
     void Update () {
 		if(Input.GetKeyDown(jumpKeyCode) || Input.GetKeyDown(altJump))
         {
-            playerJump();
+            Vector2 bottomOfPlayer = new Vector2(transform.position.x, transform.position.y - GetComponent<Collider2D>().bounds.extents.y);
+            RaycastHit2D detectGround = Physics2D.Raycast(bottomOfPlayer, Vector2.down, 0.125f);
+            if (detectGround.collider != null)
+            {
+                jumpsLeft = 2;
+            }
+                playerJump();
         }
-        if (Input.GetKeyDown(moveRightKeyCode))
+        if (Input.GetKey(moveRightKeyCode))
         {
             playerMoveX = horizontalSpeed;
         }
-        if (Input.GetKeyDown(moveLeftKeyCode))
+        if (Input.GetKey(moveLeftKeyCode))
         {
             playerMoveX = -horizontalSpeed;
         }
-        if (Input.GetKeyDown(moveLeftKeyCode) && Input.GetKeyDown(moveRightKeyCode))
+        if (Input.GetKey(moveLeftKeyCode) && Input.GetKey(moveRightKeyCode))
         {
             playerMoveX = 0;
         }
-        if (Input.GetKeyDown(fallDownKeyCode))
+        if (Input.GetKey(fallDownKeyCode))
         {
-
+            Vector2 bottomOfPlayer = new Vector2(transform.position.x, transform.position.y - GetComponent<Collider2D>().bounds.extents.y);
+            RaycastHit2D detectGlassPlatform = Physics2D.Raycast(bottomOfPlayer, Vector2.down, 0.125f, LayerMask.GetMask("Glass Platform"));
+            if(detectGlassPlatform.collider != null)
+            {
+                Physics2D.IgnoreLayerCollision(2, 8, true);
+                timeNoCollideWithGlass = 30;
+            }
         }
         if (Input.GetKeyDown(dashKeyCode))
         {
@@ -88,19 +125,31 @@ public class PlayerScript : MonoBehaviour {
         {
             playerMoveX = 0;
         }
-        if(isDashing)
+        if (isDashing)
         {
-            if(numIntervalsDashed < numDashIntervals)
+            if (numIntervalsDashed < numDashIntervals)
             {
                 Vector2 moveVector = new Vector2(transform.position.x + dashIntervalLength, transform.position.y);
                 GetComponent<Rigidbody2D>().MovePosition(moveVector);
                 numIntervalsDashed++;
-            } else
+            }
+            else
             {
                 GetComponent<Rigidbody2D>().gravityScale = 1;
                 isDashing = false;
             }
-
         }
+        if(timeNoCollideWithGlass > 0)
+        {
+            timeNoCollideWithGlass--;
+        } else if(timeNoCollideWithGlass == 0)
+        {
+            Physics2D.IgnoreLayerCollision(2, 8, false);
+        }
+    }
+    IEnumerator dashCooldown()
+    {
+        yield return new WaitForSeconds(.66f);
+        canDashCooldown = true;
     }
 }
